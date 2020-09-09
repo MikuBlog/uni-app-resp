ee1b9ad19bed4986a169cf624efa6d7eef1d6c2b
mikublog <786601672@qq.com>
2020-09-09 16:35:31 +0800

    更新代码

--- api/pay/h5_wx_pay.js
+++ api/pay/h5_wx_pay.js
@@ -1,5 +1,5 @@
 import {
-	appId
+	appid
 } from '@/global/js/baseUrl'
 /*
     微信支付方法(uni-app h5)适用
@@ -32,7 +32,7 @@ const wexinPay = (data, cb = () => {}, errorCb = () => {}, complete = () => {}) 
 
 	   wx.config({
 	       debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-	       appId, // 必填，公众号的唯一标识
+	       appId: appid, // 必填，公众号的唯一标识
 	       timestamp, // 必填，生成签名的时间戳
 	       nonceStr, // 必填，生成签名的随机串
 	       signature, // 必填，签名，见附录1
